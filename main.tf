69911fd0495905d60f91b553317eb7de361d999c
Nutan-Gare <156045142+Nutan-Gare@users.noreply.github.com>
2024-03-06 09:55:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,28 @@
 terraform {
-required_version = "~> 1.1"
+  required_version = "~> 1.1"
   required_providers {
     aws = {
       version = "~>3.27"
     }
   }
 }
+
 provider "aws" {
   region     = var.region_name
   access_key = var.access_key
   secret_key = var.secret_key
 }
- 
+
 resource "aws_instance" "yourec2" {
-  ami                    = "ami-001440bcc4ddffcf1"
-  instance_type          = "t2.micro"
+  ami           = "ami-001440bcc4ddffcf1"
+  instance_type = "t2.micro"
   tags = {
     Name = "terraform-inst"
   }
 }
-variable "access key" {}
-variable "secret key" {}
+
+variable "access_key" {}
+variable "secret_key" {}
 variable "region_name" {
   default = "ap-southeast-1"
 }
